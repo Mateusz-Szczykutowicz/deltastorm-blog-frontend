6047c90caee5cd1bd847db4fd8fdf5b31177259e
Mateusz Szczykutowicz <kontakt@deltastorm.pl>
2022-04-06 13:13:12 +0200

    Header component - refactoring

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,32 +1,56 @@
 import { faBars, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { NextPage } from "next";
+import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "../styles/Header.module.scss";
 
 const Header: NextPage = () => {
+    const router = useRouter();
     return (
         <>
-            <header className={styles.wrapper}>
-                <h1>Delta Storm</h1>
-                <nav className={styles.navWrapper}>
-                    <div className={styles.search}>
-                        <Link href="/test" passHref>
+            <div className={styles.wrapper}>
+                <header>
+                    <h1>Delta Storm</h1>
+                    <Image
+                        src={"/public/logo.svg"}
+                        width={160}
+                        height={35}
+                        // layout="fill"
+                        alt="Delta Storm"
+                        className={styles.logo}
+                        onClick={() => router.push("/")}
+                    ></Image>
+                    <nav className={styles.navWrapper}>
+                        <div className={styles.search}>
                             <FontAwesomeIcon
                                 icon={faMagnifyingGlass}
                                 className={styles.searchIcon}
+                                onClick={() => router.push("/search")}
                             />
-                        </Link>
-                    </div>
+                        </div>
 
-                    <div className={styles.hamburgerMenu}>
-                        <FontAwesomeIcon
-                            icon={faBars}
-                            className={styles.hamburgerMenuIcon}
-                        />
-                    </div>
-                </nav>
-            </header>
+                        <div className={styles.hamburgerMenu}>
+                            <FontAwesomeIcon
+                                icon={faBars}
+                                className={styles.hamburgerMenuIcon}
+                            />
+                            <ul className={styles.menu}>
+                                <li>
+                                    <Link href={"#"}>Wsparcie</Link>
+                                </li>
+                                <li>
+                                    <Link href={"#"}>Zarejestruj się</Link>
+                                </li>
+                                <li>
+                                    <Link href={"#"}>Zaloguj się</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </header>
+            </div>
         </>
     );
 };
