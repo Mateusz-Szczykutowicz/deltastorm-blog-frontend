3486de74e71e1c9ec8a4a9e97096e513b386d1b6
Mateusz Szczykutowicz <kontakt@deltastorm.pl>
2022-04-06 13:14:19 +0200

    Footer component - refactoring

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,13 +4,15 @@ import styles from "../styles/Footer.module.scss";
 
 const Footer: NextPage = () => {
     return (
-        <footer className={styles.footerWrapper}>
-            <h3>&copy; Copyright 2022 | Delta Storm</h3>
-            <p>
-                All Rigths Reserved | Created by Mateusz Szczykutowicz and
-                Karolina Adamowicz
-            </p>
-        </footer>
+        <div className={styles.footerWrapper}>
+            <footer>
+                <h3>&copy; Copyright 2022 | Delta Storm</h3>
+                <p>
+                    All Rigths Reserved | Created by Mateusz Szczykutowicz and
+                    Karolina Adamowicz
+                </p>
+            </footer>
+        </div>
     );
 };
 
